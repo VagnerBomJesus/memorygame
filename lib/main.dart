08f10cf5a864de417c90d737a3c0884c21ee9a4b
VagnerBomJesus <vagneripg@gmail.com>
2022-03-15 12:06:21 +0000

    add

---
+++ lib/main.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+import 'package:memorygame/components/info_card.dart';
+import 'package:memorygame/utils/game_utils.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  //setting text style
+  TextStyle whiteText = TextStyle(color: Colors.white);
+  bool hideTest = false;
+  Game _game = Game();
+
+  //game stats
+  int tries = 0;
+  int score = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _game.initGame();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFE55870),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              "Memory Game",
+              style: TextStyle(
+                fontSize: 48.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 24.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              info_card("Tries", "$tries"),
+              info_card("Score", "$score"),
+            ],
+          ),
+          SizedBox(
+              height: MediaQuery.of(context).size.width,
+              width: MediaQuery.of(context).size.width,
+              child: GridView.builder(
+                  itemCount: _game.gameImg!.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 16.0,
+                    mainAxisSpacing: 16.0,
+                  ),
+                  padding: EdgeInsets.all(16.0),
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        print(_game.matchCheck);
+                        setState(() {
+                          //incrementing the clicks
+                          tries++;
+                          _game.gameImg![index] = _game.cards_list[index];
+                          _game.matchCheck
+                              .add({index: _game.cards_list[index]});
+                          print(_game.matchCheck.first);
+                        });
+                        if (_game.matchCheck.length == 2) {
+                          if (_game.matchCheck[0].values.first ==
+                              _game.matchCheck[1].values.first) {
+                            print("true");
+                            //incrementing the score
+                            score += 100;
+                            _game.matchCheck.clear();
+                          } else {
+                            print("false");
+
+                            Future.delayed(Duration(milliseconds: 500), () {
+                              print(_game.gameColors);
+                              setState(() {
+                                _game.gameImg![_game.matchCheck[0].keys.first] =
+                                    _game.hiddenCardpath;
+                                _game.gameImg![_game.matchCheck[1].keys.first] =
+                                    _game.hiddenCardpath;
+                                _game.matchCheck.clear();
+                              });
+                            });
+                          }
+                        }
+                      },
+                      child: Container(
+                        padding: EdgeInsets.all(16.0),
+                        decoration: BoxDecoration(
+                          color: Color(0xFFFFB46A),
+                          borderRadius: BorderRadius.circular(8.0),
+                          image: DecorationImage(
+                            image: AssetImage(_game.gameImg![index]),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    );
+                  }))
+        ],
+      ),
+    );
+  }
+}
